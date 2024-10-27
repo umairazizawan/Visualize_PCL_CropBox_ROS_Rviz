31bb6d0c06b6e2aaa926ea9636e85dd24adb2b9b
umairazizawan <umairazizawan66@gmail.com>
2024-10-27 17:35:00 +0500

    Adjusted code formatting

--- README.md
+++ README.md
@@ -1,22 +1,36 @@
 # Visualize_PCL_CropBox_ROS_Rviz
-Visualize_PCL_CropBox_ROS_Rviz is presents an approach For Debugging Transformation (TF) issues when working with PCL CropBox in ROS. 
 
-PCL Cropbox only keeps the points which lye inside its bounds. But in case the coordinates and transformation of the Cropbox are not correct, it is difficult to find the location where the cropbox is actually being created and therefore correct the issue. 
-This is especially the case when working with tf trees obtained from a client, customer or a third party, as they may not be completely accurate and it is difficult and time consiming to correct them. In this case we ideally want a way to visualize the location of the cropbox and use this as the initial point for debugging. 
+`Visualize_PCL_CropBox_ROS_Rviz` presents an approach For Debugging Transformation (TF) issues when working with `PCL CropBox` in ROS. 
 
+`PCL Cropbox` only keeps the portion of the point cloud which lies inside its bounds. 
+If the coordinates and transformation of the Cropbox are incorrect, it can be difficult to correct the issue, especially when working with initial drafts of URDF files from clients and third parties.
+
+In these situations, we ideally want a way to visualize the cropbox itself and use this as the initial point for debugging. 
 One Approach to solve this issue is presented below.
 
-## Debugging Approach
+## Approach
 
-1. Fill a section of the 3D Space in Rviz with a cubic point cloud using The script cubic_point_cloud_publisher.py.
+1. Create a Ros Node which publishes a Cubic Point cloud to fill a section of the 3D Space in Rviz. The script `cubic_point_cloud_publisher.py`.
 
-2. Provide this point cloud, to the node using PCL Cropbox.
+2. Provide this point cloud message to your ROS node which is using PCL Cropbox.
 
-3. If the section of Rviz contains your cropbox it should become visible in Rviz. If not adjust the `cubic_point_cloud_publisher.py.` to cover a different section of the 3D Space.
+3. If the section of Rviz contains your Cropbox it should become visible in Rviz. If not adjust the size, location and spacing of the Cube until the Cropbox becomes visible. 
 
+## Cubic_point_cloud_publisher
+
+1. The `cubic_point_cloud_publisher.py.` publishes Cubic point cloud on the topic `/point_cloud`, which covers a section of the 3D Space in Rviz. Please use the `CUBE_SIZE` and `CUBE_SPACING` variables to change the size and density of the point cloud, respectively.
+To change the coordinates of the cube please vary the upper and lower bound on np.arrage used for the calculation x,y and z coordinates, as required.   
+
+*NOTE*: Please be careful when changing `CUBE_SIZE` and `CUBE_SPACING` variables, as rendering a large point cloud requires alot of processing power.
 
 
 ## Supported Platforms
 
-These scripts were tested on Linux (specifically UBUNTU 20) with ROS NEOTIC
-But they should be valid for other version of ROS1 which support PointCloud2 msg and other Linux Distro.
+```
+    Linux
+```
+## ROS Version
+```
+    ROS1
+```
+*NOTE:* These scripts were tested on ROS NEOTIC But they should be valid for other version of ROS1 which support PointCloud2 msg.
--- cubic_point_cloud_publisher.py
+++ cubic_point_cloud_publisher.py
@@ -7,30 +7,34 @@ import sensor_msgs.point_cloud2 as pc2
 from std_msgs.msg import Header
 
 
-##############################   CONSTANTS   ######################################################
-DEFAULT_CUBE_SIZE       = 10
-DEFAULT_CUBE_SPACING    = 0.5
+##############################  PARAMETERS  #######################################################
+CUBE_SIZE               = 10
+CUBE_SPACING            = 0.5
 
 NODE_NAME               = 'point_cloud_publisher'
 ROS_TOPIC_NAME          = '/point_cloud'
+
 PUBLISH_RATE_HZ         = 1
 ROS_TOPIC_QUEUE         = 10
+
 MSG_FRAME_ID            = "map"
 
 ###################################################################################################
 def generate_cubic_point_coordinates(cube_size, spacing):
     '''
     This function generates an array of points covering the coordinates of a cube.
-    The cube will cover an equal length in positive and negative coordinates along all axes   
+    The cube will cover an equal length in positive and negative coordinates along all axes.
+    To change the coordinates of the cube please vary the upper and lower bound on np.arrage used
+    for the calculation x,y and z coordinates as required.
     Parameters
     ----------
     cube_size   : float
         Length of one side of a cube.
-    spacing     : float 
+    spacing     : float
         Distance between points
 
-    Caution: Please be careful when adjusting these Parameters, as rendering all these points
-    can be slow down your machine.
+    NOTE: Please be careful when adjusting these Parameters, as rendering a large point cloud points
+    can slow down your machine.
 
     '''
     # Create a grid of equally spaced points within the space
@@ -40,7 +44,7 @@ def generate_cubic_point_coordinates(cube_size, spacing):
 
     # Generate a 3D grid of points to get all possible combination/coordinates inside the space
     xv, yv, zv = np.meshgrid(x, y, z, indexing='xy')
-    
+
     # Flatten the grid to a list of (x, y, z) points
     points = np.column_stack((xv.flatten(), yv.flatten(), zv.flatten()))
 
@@ -57,29 +61,27 @@ def publish_cubic_point_cloud(pub):
     header.frame_id = MSG_FRAME_ID 
 
     # Generate points with a specified length and spacing
-    points = generate_cubic_point_coordinates(DEFAULT_CUBE_SIZE, DEFAULT_CUBE_SPACING)
-    # Create a PointCloud2 message
+    points = generate_cubic_point_coordinates(CUBE_SIZE, CUBE_SPACING)
+
     point_cloud_msg = pc2.create_cloud_xyz32(header, points.tolist())
 
-    # Publish the message
     pub.publish(point_cloud_msg)
 
 ###################################################################################################
 def point_cloud_publisher():
 
-    # Setup ros node
     rospy.init_node(NODE_NAME, anonymous=True)
 
     pub = rospy.Publisher(ROS_TOPIC_NAME, PointCloud2, queue_size=ROS_TOPIC_QUEUE)
 
-    rate = rospy.Rate(PUBLISH_RATE_HZ) 
+    rate = rospy.Rate(PUBLISH_RATE_HZ)
 
     while not rospy.is_shutdown():
-        
-        # Generate and  publish the cubic point cloud
+
         publish_cubic_point_cloud(pub)
         rate.sleep()
 
+###################################################################################################
 if __name__ == '__main__':
     try:
         point_cloud_publisher()
